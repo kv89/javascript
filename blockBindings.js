b06fbef330eb9e787423f35f2dfc194742be2268
kv89 <kalaskarvishald@gmail.com>
2016-10-05 09:58:38 +0530

    Update blockBindings.js

--- blockBindings.js
+++ blockBindings.js
@@ -6,7 +6,19 @@ for(var i=0; i<10; i++){// var does hoisting:
         console.log( i );
     });
 }
+// -------------------------------------------------------
 
+var funsJ = [];
+// fix for var 
+for(var j = 0; j < 10; j++){
+    funsJ.push(function( num ){
+        return function(){
+            console.log( num );
+        };
+    }( j ));
+}
+
+// -------------------------------------------------------
 var funsL = [];
 // now using let:
 for(let i=0; i<10; i++){// let doesn't gets hoisted:
@@ -15,11 +27,19 @@ for(let i=0; i<10; i++){// let doesn't gets hoisted:
     });
 }
 
+// -------------------------------------------------------
 console.log('using \'var\': ');
 funs.forEach(function( f ){
     f();
 })
 
+// -------------------------------------------------------
+console.log('using \'var - fixed\':');
+funsJ.forEach(function( f ){
+    f();
+});
+
+// -------------------------------------------------------
 console.log('using \'let\': ');
 funsL.forEach(function( f ){
     f();
